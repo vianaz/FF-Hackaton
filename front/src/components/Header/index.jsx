2072342373f5608ca9cdc74770ffaa919f0839ff
andreseichi <andre_seichi@hotmail.com>
2022-06-04 16:29:40 -0300

    Header component

---
+++ front/src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import { Container, LogoTitle, Profile } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <LogoTitle>logo aqui</LogoTitle>
+
+      <Profile />
+    </Container>
+  );
+}
---
+++ front/src/components/Header/styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  background-color: beige;
+  width: 100%;
+  padding: 1rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  position: sticky;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 5;
+`;
+
+export const LogoTitle = styled.h1`
+  font-size: 2rem;
+`;
+
+export const Profile = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: aquamarine;
+
+  cursor: pointer;
+`;
