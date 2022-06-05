86878c536a481da8a34e1f74606e34b2163e9c9c
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2022-06-05 10:28:27 -0300

    save changes

---
+++ front/src/components/personalInfoComponents/personalInfoComponent.jsx
@@ -0,0 +1,3 @@
+export default function PersonalComponent() {
+    return <div>oi</div>
+}
---
+++ front/src/pages/personal/index.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export default function PersonalInfo() {
+    return <Main></Main>
+}
+
+const Main = styled.main`
+  min-width: 100vw;
+  min-height: 100vh;
+  background-color: #329752;
+`;
